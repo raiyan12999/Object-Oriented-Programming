a15f270e34531c667deaa35b2a6fdca2f912ba40
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-11-06 22:45:01 -0800

    classes

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,27 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello!");
+
+        Student student1 = new Student();
+        System.out.println(student1.rno);
+        System.out.println(student1.name);
+        System.out.println(student1.marks);
+
+        student1.rno = 12999;
+        student1.name = "Raiyan";
+        student1.marks = 89;
+
+        System.out.println(student1.rno);
+        System.out.println(student1.name);
+        System.out.println(student1.marks);
     }
+}
+
+class Student {
+
+    int rno;
+    String name;
+    int marks;
+
+
 }
