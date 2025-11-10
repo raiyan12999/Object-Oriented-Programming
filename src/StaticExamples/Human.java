242499c29b8dc93a7aa79034fc1b3fc87b75f969
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-11-10 06:25:30 -0800

    use of static variable

--- src/StaticExamples/Human.java
+++ src/StaticExamples/Human.java
@@ -6,11 +6,13 @@ public class Human {
     String name;
     int Salary;
     boolean married;
+    static long population;
 
     Human(int age, String name, int salary, boolean married){
         this.age = age;
         this.name = name;
         this.Salary = salary;
         this.married = married;
+        Human.population += 1;
     }
 }
--- src/StaticExamples/Main.java
+++ src/StaticExamples/Main.java
@@ -3,9 +3,10 @@ package StaticExamples;
 public class Main {
     public static void main(String[] args) {
         Human Raiyan = new Human(27, "Raiyan", 25000, true);
+        System.out.println(Raiyan.population);
         Human Radid = new Human(19, "Radid", 25, false);
 
-        System.out.println(Raiyan.married);
-        System.out.println(Radid.married);
+
+        System.out.println(Radid.population);
     }
 }
