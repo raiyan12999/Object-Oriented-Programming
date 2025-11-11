9f5baf4c83ac0136d49998f58d88b338f602b207
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-11-11 02:58:56 -0800

    created a base class Box

---
+++ src/properties/inheritance/Box.java
@@ -0,0 +1,20 @@
+package properties.inheritance;
+
+public class Box {
+
+    double l;
+    double w;
+    double h;
+
+    void Information(){
+        System.out.println("Length of Box: " + this.l + " width: " + this.w + " height: " + this.h);
+    }
+
+    Box(double length, double width, double height){
+
+        this.l = length;
+        this.w = width;
+        this.h = height;
+
+    }
+}
---
+++ src/properties/inheritance/Main.java
@@ -0,0 +1,10 @@
+package properties.inheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Box box = new Box(4, 6, 6);
+        box.Information();
+    }
+}
