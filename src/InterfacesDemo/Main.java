cb091e4a60ce21bccbb168085851daa9554224aa
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-11-19 09:25:37 -0800

    class inside class

--- src/InterfacesDemo/Main.java
+++ src/InterfacesDemo/Main.java
@@ -3,6 +3,14 @@ package InterfacesDemo;
 public class Main {
     public static void main(String[] args) {
 
+        class Test2{
+            int b;
+
+            Test2(int b){
+                this.b = b;
+            }
+        }
+
         Computer computer = new Computer();
         computer.processData();
         computer.storesData();
@@ -10,7 +18,12 @@ public class Main {
 
         Test test = new Test(45);
         System.out.println(test.a);
+
+        Test2 test2 = new Test2(67);
+        System.out.println(test2.b);
     }
+
+
 }
 
 class Test{
