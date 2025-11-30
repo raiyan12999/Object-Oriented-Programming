d3f50151b0afb0310414bcd2f7d9a849c6e7a938
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-11-30 03:21:01 -0800

    starting comparable

---
+++ src/Generics/Comparable/Main.java
@@ -0,0 +1,10 @@
+package Generics.Comparable;
+
+public class Main {
+    public static void main(String[] args) {
+        Student raiyan = new Student(17, 89.5f);
+        Student afifa = new Student(28, 96.3f);
+
+
+    }
+}
---
+++ src/Generics/Comparable/Student.java
@@ -0,0 +1,12 @@
+package Generics.Comparable;
+
+public class Student {
+
+    int rollno;
+    float marks;
+
+    public Student(int rollno, float marks){
+        this.rollno = rollno;
+        this.marks = marks;
+    }
+}
